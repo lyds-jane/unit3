c090b8a3b7018a77942918296f71832467101786
lyds-jane <52633187+lyds-jane@users.noreply.github.com>
2020-04-06 22:33:20 -0400

    Create dogsAge.py

---
+++ week28/dogsAge.py
@@ -0,0 +1,12 @@
+# This program calculates a dog's age
+
+humanYears = int(input("Enter the dog's age in human years: "))
+dogYears = 0
+
+if humanYears <= 2:
+    dogYears = humanYears * 10.5
+else:
+    dogYears += (humanYears - 2) * 4
+    dogYears += 21
+
+print(dogYears)
