6713a4caf6c80350de2647faa4087718ef6c99ef
lyds-jane <52633187+lyds-jane@users.noreply.github.com>
2020-04-07 22:04:14 -0400

    Create alphabetize.py

---
+++ week28/alphabetize.py
@@ -0,0 +1,11 @@
+# get the string from the user
+string = input("Enter the text")
+
+# split the string
+x = string.split(",")
+
+# alphabatize the list
+x.sort()
+
+# print the sorted list
+print(x)
