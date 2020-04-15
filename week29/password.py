e0a1809d4fd5792083de70f80528cc5eac28e08c
lyds-jane <52633187+lyds-jane@users.noreply.github.com>
2020-04-14 20:08:30 -0400

    Update password.py

--- week29/password.py
+++ week29/password.py
@@ -1,10 +1,10 @@
 import random
 results = []
-
-for i in input("How many passwords would you like?"):
+times = int(input("How many passwords would you like?"))
+for i in range(times):
     password = list()
     for a in range(20):
-        ref = int(random.randrange(33,176,1))
+        ref = int(random.randrange(33,126,1))
         add = str(chr(ref))
         password.append(add)
     pwd = "".join(password)
